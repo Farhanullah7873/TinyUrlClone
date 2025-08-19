bb59039aec4686e06194ee605e002646a61a2ce7
farhan ullah <farhanullahwazir7873@gmail.com>
2025-08-19 09:34:46 +0500

    Tinyurl

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import { ChevronDownIcon } from '@heroicons/react/20/solid'
 import { Disclosure, DisclosureButton, DisclosurePanel, Menu, MenuButton, MenuItem, MenuItems } from '@headlessui/react'
 import { Bars3Icon, BellIcon, XMarkIcon } from '@heroicons/react/24/outline'
 import Header from './component/Header'
-import { HomeBody } from './component/bodyComponent/homeBody'
+import { HomeBody } from '../component/bodyComponent/HomeBody'
 function App() {
 return(
  <>
--- src/pages/Branded.jsx
+++ src/pages/Branded.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Header from '../component/Header'
-import { HomeBody } from '../component/bodyComponent/homeBody'
+import { HomeBody } from './component/bodyComponent/HomeBody'
+
 export const BrandednCustomizedLinks = () => {
   return(
     <div><Header/>
