470405566c0738505a0c4fcea0d369dd940ace16
farhan ullah <farhanullahwazir7873@gmail.com>
2025-08-19 09:15:47 +0500

    Update Branded.jsx

--- src/pages/Branded.jsx
+++ src/pages/Branded.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
 import Header from '../component/header'
+import { HomeBody } from '../component/bodyComponent/homeBody'
 export const BrandednCustomizedLinks = () => {
   return(
     <div><Header/>
-    <p>dsf</p>
+  <HomeBody/>
     </div>
     
   )
